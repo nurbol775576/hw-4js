3a484278977b2081076be1ded4fbf061cb5d096e
nurbol775576 <nurboltazibaev14@gmail.com>
2023-03-07 20:42:52 +0600

    hw-js4

---
+++ app.js
@@ -0,0 +1,19 @@
+const arr = [{
+name: "Almaz",
+lastName:"ormonbekov",
+age:"13"
+},{
+ name:"Aziz",
+ lastName: "Nematov",
+age: "14"
+},{
+ name:"Bayaz",
+ lastName:"Bolotov",
+ age: "17"
+}]
+
+
+console.log(arr[0].name, arr[1].name, arr[2].name);
+
+
+for(let num=0; num<=20; num+=1 ) console.log(num);
